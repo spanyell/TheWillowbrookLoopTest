a81e72129f2dcad18e3d346403475f395fb8853c
Dan Beers <spanyell@icloud.com>
2025-06-26 18:33:31 -0700

    Initial Commit

---
+++ TheWillowbrookLoopTest/TheWillowbrookLoopTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheWillowbrookLoopTestApp.swift
+//  TheWillowbrookLoopTest
+//
+//  Created by Dan Beers on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TheWillowbrookLoopTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
